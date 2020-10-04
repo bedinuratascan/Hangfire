7f716e05c924d0e44cd97f40dff6c8d8e4349877
bedinuratascan <bedinuratascan@gmail.com>
2020-10-04 20:49:51 +0300

    Recurring job

---
+++ Hangfire/BackgroundJobs/RecurringJobs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangfire.BackgroundJobs
+{
+    public class RecurringJobs
+    {
+        public static void ReportingJob()
+        {
+            Hangfire.RecurringJob.AddOrUpdate("reportjob", () => EmailReport(),Cron.Minutely);
+        }
+
+        public static void EmailReport()
+        {
+            Debug.WriteLine("Rapor, email olarak gönderildi.");
+        }
+
+    }
+}
